12e362914b4575e9678c9cec26d8d4a766f98650
adorno <pch4623@gmail.com>
2020-10-22 21:58:21 +0900

    순수 자바로 프로젝트 시작

---
+++ src/test/java/learnspring/Springcore/SpringCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package learnspring.Springcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
