bdf8586ad35ced9a05ca31f8a8b34699878c1a86
adorno <pch4623@gmail.com>
2020-10-23 23:25:42 +0900

    메모리DB 사용시 구현 클래스 생성

--- src/main/java/learnspring/member/MemoryMemberRepository.java
+++ src/main/java/learnspring/member/MemoryMemberRepository.java
@@ -1,2 +1,19 @@
-package learnspring.member;public class MemoryMemberRepository {
+package learnspring.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
 }
