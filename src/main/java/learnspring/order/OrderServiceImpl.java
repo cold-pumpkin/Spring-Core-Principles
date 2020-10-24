35645e75d87e4fefa9553c23d0c62ae648ca14f6
adorno <pch4623@gmail.com>
2020-10-24 23:50:06 +0900

    주문 서비스 인터페이스 구현체 작성

--- src/main/java/learnspring/order/OrderServiceImpl.java
+++ src/main/java/learnspring/order/OrderServiceImpl.java
@@ -1,2 +1,22 @@
-package learnspring.order;public class OrderServiceImpl {
+package learnspring.order;
+
+import learnspring.discount.DiscountPolicy;
+import learnspring.discount.FixedDiscountPolicy;
+import learnspring.member.Member;
+import learnspring.member.MemberRepository;
+import learnspring.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixedDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        // 회원 찾아서 할인 정책 적용 후 주문 객체 리턴
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
 }
