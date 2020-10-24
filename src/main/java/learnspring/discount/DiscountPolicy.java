2ae3cf765c6c2a1f5f277d43f420cc82a892f052
adorno <pch4623@gmail.com>
2020-10-24 23:49:10 +0900

    할인 정채 인터페이스 & 정액정책 구현

---
+++ src/main/java/learnspring/discount/DiscountPolicy.java
@@ -0,0 +1,11 @@
+package learnspring.discount;
+
+import learnspring.member.Member;
+
+public interface DiscountPolicy {
+
+    /**
+     * @return 할인 대상 금액
+     */
+    int discount(Member member, int price);
+}
--- src/main/java/learnspring/discount/FixedDiscountPolicy.java
+++ src/main/java/learnspring/discount/FixedDiscountPolicy.java
@@ -1,2 +1,17 @@
-package learnspring.discount;public class FixedDiscountPolicy {
+package learnspring.discount;
+
+import learnspring.member.Grade;
+import learnspring.member.Member;
+
+public class FixedDiscountPolicy implements DiscountPolicy {
+
+    private final int discountFixedAmount = 1000;  // 1000원 고정 할인금액
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return discountFixedAmount;
+        }
+        return 0;
+    }
 }
