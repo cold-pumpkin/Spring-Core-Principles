bedbe07411397b64c164d138d1f062736fbbced8
adorno <pch4623@gmail.com>
2020-10-24 23:50:26 +0900

    Main에서 직접 주문 테스트

--- src/main/java/learnspring/Springcore/OrderApp.java
+++ src/main/java/learnspring/Springcore/OrderApp.java
@@ -1,2 +1,25 @@
-package learnspring.Springcore;public class OrderApp {
+package learnspring.Springcore;
+
+import learnspring.member.Grade;
+import learnspring.member.Member;
+import learnspring.member.MemberService;
+import learnspring.member.MemberServiceImpl;
+import learnspring.order.Order;
+import learnspring.order.OrderService;
+import learnspring.order.OrderServiceImpl;
+
+public class OrderApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        // 회원 등록 후 주문
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        System.out.println(order);
+        System.out.println(order.calculatePrice());
+    }
 }
