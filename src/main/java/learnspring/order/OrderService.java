622c8338acd4a17ebe0b29d5d3d6b71691a9e0dd
adorno <pch4623@gmail.com>
2020-10-24 23:49:23 +0900

    주문 서비스 인터페이스 작성

--- src/main/java/learnspring/order/OrderService.java
+++ src/main/java/learnspring/order/OrderService.java
@@ -1,2 +1,6 @@
-package learnspring.order;public interface OrderService {
+package learnspring.order;
+
+public interface OrderService {
+
+    Order createOrder(Long memberId, String itemName, int itemPrice);
 }
