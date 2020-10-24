344a31ca0728f6951dd5c193cc6e6dbff0feb5bc
adorno <pch4623@gmail.com>
2020-10-24 23:50:45 +0900

    주문 서비스 테스트 코드 작성

--- src/test/java/learnspring/order/OrderServiceTest.java
+++ src/test/java/learnspring/order/OrderServiceTest.java
@@ -1,2 +1,28 @@
-package learnspring.order;public class OrderServiceTest {
+package learnspring.order;
+
+import learnspring.member.Grade;
+import learnspring.member.Member;
+import learnspring.member.MemberService;
+import learnspring.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.*;
+
+public class OrderServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        // given
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+
+        // when : 회원 가입 후 주문 등록
+        memberService.join(member);
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        // then
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+
 }
