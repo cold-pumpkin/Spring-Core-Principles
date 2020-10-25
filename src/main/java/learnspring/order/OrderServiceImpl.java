56efcfdd533319848fa02dedff45d2890d2cbc1b
adorno <pch4623@gmail.com>
2020-10-26 00:08:47 +0900

    할인 정책 변경에 의한 서비스 클라이언트 코드 변경
    OCP(개방폐쇄원칙), DIP(의존역전원칙) 위반. 정책 변경이 있더라도
    클라이언트 코드를 수정할 필요 없도록 DiscountPolicy의 구현객체를 대신
    생성해 주입해줄 무언가가 필요함.

--- src/main/java/learnspring/order/OrderServiceImpl.java
+++ src/main/java/learnspring/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package learnspring.order;
 
 import learnspring.discount.DiscountPolicy;
 import learnspring.discount.FixedDiscountPolicy;
+import learnspring.discount.RateDiscountPolicy;
 import learnspring.member.Member;
 import learnspring.member.MemberRepository;
 import learnspring.member.MemoryMemberRepository;
@@ -9,7 +10,11 @@ import learnspring.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixedDiscountPolicy();
+
+    /** 할인 정책 변경 시 클라이언트 코드를 바꿔야하는 문제 발생 **/
+    //private final DiscountPolicy discountPolicy = new FixedDiscountPolicy();
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
