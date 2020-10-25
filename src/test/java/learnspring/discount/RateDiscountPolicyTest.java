81f0b9363de04cb22ef2f16dd121c7e8d0282969
adorno <pch4623@gmail.com>
2020-10-25 23:28:11 +0900

    할인정책 적용 테스크 코드 작성

---
+++ src/test/java/learnspring/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,39 @@
+package learnspring.discount;
+
+import learnspring.member.Grade;
+import learnspring.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.*;
+
+import static org.assertj.core.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP는 10% 할인이 적용되어야 한다.")
+    void VIP_discount() {
+        // given
+        Member member = new Member(1L, "memberVIP", Grade.VIP);
+
+        // when
+        int discount = discountPolicy.discount(member, 10000);
+
+        // then
+        assertThat(discount).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("VIP가 아니면 10% 할인이 적용되지 않는다.")
+    void BASIC_discount() {
+        // given
+        Member member = new Member(2L, "memberBASIC", Grade.BASIC);
+
+        // when
+        int discount = discountPolicy.discount(member, 10000);
+
+        // then
+        assertThat(discount).isEqualTo(0);
+    }
+}
