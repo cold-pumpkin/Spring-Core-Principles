fbc80492ae8ef384ea1602ba438a6f1e177d8869
adorno <pch4623@gmail.com>
2020-10-26 23:13:29 +0900

    인터페이스에만 의존하도록 수정 (생성자를 통해 의존관계 주입)

--- src/main/java/learnspring/member/MemberServiceImpl.java
+++ src/main/java/learnspring/member/MemberServiceImpl.java
@@ -2,7 +2,14 @@ package learnspring.member;
 
 public class MemberServiceImpl implements MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    // 인터페이스에만 의존하도록 수정
+    private final MemberRepository memberRepository;
+
+    // 생성자를 통한 의존관계 주입 (AppConfig를 통해 구현체가 주입됨)
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
     @Override
     public void join(Member member) {
         memberRepository.save(member);
