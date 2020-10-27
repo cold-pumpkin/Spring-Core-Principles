3e970a1cf629d40617267394a6ab8a66d6262d4d
adorno <pch4623@gmail.com>
2020-10-27 20:54:17 +0900

    중복을 제거하고, 역할에 따른 구현체가 드러나도록 리팩토링

--- src/main/java/learnspring/Springcore/AppConfig.java
+++ src/main/java/learnspring/Springcore/AppConfig.java
@@ -1,19 +1,30 @@
 package learnspring.Springcore;
 
+import learnspring.discount.DiscountPolicy;
 import learnspring.discount.FixedDiscountPolicy;
+import learnspring.member.MemberRepository;
 import learnspring.member.MemberService;
 import learnspring.member.MemberServiceImpl;
 import learnspring.member.MemoryMemberRepository;
 import learnspring.order.OrderService;
 import learnspring.order.OrderServiceImpl;
 
-public class AppConfig {
+public class AppConfig {  // 역할에 따른 구현으로 리팩토링
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();  // 구현체 바뀌면 이 부분만 수정
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixedDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());  // 구현체 바뀌면 이 부분만 수정
     }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixedDiscountPolicy();
+    }
+
 }
