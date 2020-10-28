6b8d21206a5809e3df1795ec4b1361d0a57c2283
adorno <pch4623@gmail.com>
2020-10-28 23:51:06 +0900

    스프링 컨테이너에서 서비스 객체 꺼내오기

--- src/main/java/learnspring/Springcore/MemberApp.java
+++ src/main/java/learnspring/Springcore/MemberApp.java
@@ -4,15 +4,22 @@ import learnspring.member.Grade;
 import learnspring.member.Member;
 import learnspring.member.MemberService;
 import learnspring.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
 
+        // ** 1) 직접 서비스 생성
         //MemberService memberService = new MemberServiceImpl();
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+        // ** 2) AppConfig를 통해 의존성 주입
+        //AppConfig appConfig = new AppConfig();
+        //MemberService memberService = appConfig.memberService();
 
+        // ** 3) 스프링 컨테이너 활용
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
         // 회원 가입
         Member member = new Member(1L, "memberA", Grade.VIP);
--- src/main/java/learnspring/Springcore/OrderApp.java
+++ src/main/java/learnspring/Springcore/OrderApp.java
@@ -7,16 +7,25 @@ import learnspring.member.MemberServiceImpl;
 import learnspring.order.Order;
 import learnspring.order.OrderService;
 import learnspring.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
 
+        // ** 1) 직접 서비스 생성
         //MemberService memberService = new MemberServiceImpl();
         //OrderService orderService = new OrderServiceImpl();
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        // ** 2) AppConfig를 통해 의존성 주입
+        //AppConfig appConfig = new AppConfig();
+        //MemberService memberService = appConfig.memberService();
+        //OrderService orderService = appConfig.orderService();
+
+        // ** 3) 스프링 컨테이너 활용
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         // 회원 등록 후 주문
         Long memberId = 1L;
