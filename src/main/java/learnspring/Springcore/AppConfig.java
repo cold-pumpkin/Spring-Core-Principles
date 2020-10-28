4f252ba13d8015b5ecc9e533ea2d3673dc28cd07
adorno <pch4623@gmail.com>
2020-10-28 23:49:35 +0900

    어노테이션으로 설정 파일 지정 및 스프링 컨테이너에 빈 등록

--- src/main/java/learnspring/Springcore/AppConfig.java
+++ src/main/java/learnspring/Springcore/AppConfig.java
@@ -9,21 +9,28 @@ import learnspring.member.MemberServiceImpl;
 import learnspring.member.MemoryMemberRepository;
 import learnspring.order.OrderService;
 import learnspring.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {  // 역할에 따른 구현으로 리팩토링
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();  // 구현체 바뀌면 이 부분만 수정
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());  // 구현체 바뀌면 이 부분만 수정
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         //return new FixedDiscountPolicy();
         return new RateDiscountPolicy();
