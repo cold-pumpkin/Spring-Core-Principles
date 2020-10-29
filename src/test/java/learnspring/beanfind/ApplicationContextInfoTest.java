f878129c3a1f3f6ebe54fbe8bb9ef521d6645d8a
adorno <pch4623@gmail.com>
2020-10-29 21:53:15 +0900

    스프링 컨테이너에 등록된 빈 확인 테스트 코드 작성

---
+++ src/test/java/learnspring/beanfind/ApplicationContextInfoTest.java
@@ -0,0 +1,34 @@
+package learnspring.beanfind;
+
+import learnspring.Springcore.AppConfig;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextInfoTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("등록된 모든 빈 출력하기")
+    void findAllBeans() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("bean name = " + beanDefinitionName + " object = " + bean);
+        }
+    }
+
+    @Test
+    @DisplayName("직접 등록한 애플리케이션 빈 출력하기")
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("bean name = " + beanDefinitionName + " object = " + bean);
+            }
+        }
+    }
+}
