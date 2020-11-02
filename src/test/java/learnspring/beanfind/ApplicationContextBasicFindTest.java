778c4a015d690e004969df005babac4b16781703
adorno <pch4623@gmail.com>
2020-11-02 21:57:08 +0900

    같은 타입의 빈이 여러개 등록되어 있을 시 테스트 코드 작성

--- src/test/java/learnspring/beanfind/ApplicationContextBasicFindTest.java
+++ src/test/java/learnspring/beanfind/ApplicationContextBasicFindTest.java
@@ -1,12 +1,19 @@
 package learnspring.beanfind;
 
 import learnspring.Springcore.AppConfig;
+import learnspring.member.MemberRepository;
 import learnspring.member.MemberService;
 import learnspring.member.MemberServiceImpl;
+import learnspring.member.MemoryMemberRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -40,6 +47,47 @@ public class ApplicationContextBasicFindTest {
     @DisplayName("빈 이름으로 조회 실패")
     void findBeanByNameFail() {
         assertThrows(NoSuchBeanDefinitionException.class, () -> ac.getBean("XXX", MemberService.class));
+    }
+
+    // 새로운 설정파일
+    AnnotationConfigApplicationContext ac2 = new AnnotationConfigApplicationContext(SameTypeBeanConfig.class);
+
+    @Test
+    @DisplayName("타입으로 조회 시 같은 타입이 둘 이상인 경우, 타입으로 호출 시 중복 오류 발생")
+    void findDuplicatedTypeBean() {
+        assertThrows(NoUniqueBeanDefinitionException.class, () -> ac2.getBean(MemberRepository.class));
+    }
+
+    @Test
+    @DisplayName("타입으로 조회 시 같은 타입이 둘 이상인 경우, 빈 이름을 지정하여 호출")
+    void findDuplicatedTypeBeanByName() {
+        MemberRepository memberRepository = ac2.getBean("memberRepository1", MemberRepository.class);
+        assertThat(memberRepository).isInstanceOf(MemberRepository.class);
+    }
+
+    @Test
+    @DisplayName("특정 타입의 빈 모두 조회하기")
+    void findAllBeanByType() {
+        Map<String, MemberRepository> beansOfType = ac2.getBeansOfType(MemberRepository.class);
+        for(String key : beansOfType.keySet()) {
+            System.out.println("key = " + key + " value = " + beansOfType.get(key));
+        }
+        assertThat(beansOfType.size()).isEqualTo(2);
+    }
+
+    @Configuration
+    static class SameTypeBeanConfig {
+
+        // 같은 타입의 빈 두개 등록
+        @Bean
+        public MemberRepository memberRepository1() {
+            return new MemoryMemberRepository();
+        }
+
+        @Bean
+        public MemberRepository memberRepository2() {
+            return new MemoryMemberRepository();
+        }
     }
 
 }
