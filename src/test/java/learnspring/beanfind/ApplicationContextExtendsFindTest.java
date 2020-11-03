f78eb87824a7f289752dca6391d9a7127afc4de6
adorno <pch4623@gmail.com>
2020-11-03 23:02:10 +0900

    부모타입을 이용해 빈 조회 테스트 코드

---
+++ src/test/java/learnspring/beanfind/ApplicationContextExtendsFindTest.java
@@ -0,0 +1,64 @@
+package learnspring.beanfind;
+
+import learnspring.discount.DiscountPolicy;
+import learnspring.discount.FixedDiscountPolicy;
+import learnspring.discount.RateDiscountPolicy;
+import net.bytebuddy.build.ToStringPlugin;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ApplicationContextExtendsFindTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+
+    @Test
+    @DisplayName("부모 타입을 조회 시, 자식이 둘 이상 있으면 중복 오류가 발생")
+    void findDuplicatedBeanByParentType() {
+        assertThrows(NoUniqueBeanDefinitionException.class, () -> ac.getBean(DiscountPolicy.class));
+    }
+
+    @Test
+    @DisplayName("부모 타입을 조회 시, 자식이 둘 이상 있으면 이름을 지정")
+    void findDuplicatedBeanByName() {
+        DiscountPolicy rateDiscountPolicy = ac.getBean("rateDiscountPolicy", DiscountPolicy.class);
+        assertThat(rateDiscountPolicy).isInstanceOf(RateDiscountPolicy.class);
+    }
+
+    @Test
+    @DisplayName("부모 타입을 조회 시, 자식이 둘 이상 있으면 특정 서브 타입 지정")
+    void findDuplicatedBeanBySubType() {
+        DiscountPolicy rateDiscountPolicy = ac.getBean(RateDiscountPolicy.class);
+        assertThat(rateDiscountPolicy).isInstanceOf(RateDiscountPolicy.class);
+    }
+
+    @Test
+    @DisplayName("부모 타입으로 모두 조회")
+    void findAllBeansByParentType() {
+        Map<String, DiscountPolicy> beansOfType = ac.getBeansOfType(DiscountPolicy.class);
+        assertThat(beansOfType.size()).isEqualTo(2);
+        for (String key : beansOfType.keySet()) {
+            System.out.println("key = " + key + " value = " + beansOfType.get(key));
+        }
+    }
+
+
+    @Configuration
+    static class TestConfig {
+        @Bean
+        public DiscountPolicy rateDiscountPolicy() {
+            return new RateDiscountPolicy();
+        }
+
+        @Bean
+        public DiscountPolicy fixedDiscountPolicy() {
+            return new FixedDiscountPolicy();
+        }
+    }
+}
