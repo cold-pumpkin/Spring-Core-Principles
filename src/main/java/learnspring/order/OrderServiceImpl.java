ac833a1aa92f2f1b1a4f4b565923286434ec8d87
adorno <pch4623@gmail.com>
2020-11-13 20:46:51 +0900

    롬복 @RequiredArgsConstructor로 자동 생성자 주입

--- src/main/java/learnspring/order/OrderServiceImpl.java
+++ src/main/java/learnspring/order/OrderServiceImpl.java
@@ -3,10 +3,12 @@ package learnspring.order;
 import learnspring.discount.DiscountPolicy;
 import learnspring.member.Member;
 import learnspring.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     // 인터페이스에만 의존하도록 수정
@@ -14,11 +16,12 @@ public class OrderServiceImpl implements OrderService {
     private final DiscountPolicy discountPolicy;
 
     //@Autowired
+    /*
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
-
+    */
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         // 회원 찾아서 할인 정책 적용 후 주문 객체 리턴
