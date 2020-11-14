6c8408ffe7fc3c70aa59bed31763a291ace90c19
adorno <pch4623@gmail.com>
2020-11-14 22:38:20 +0900

    같은 타입의 빈 여러개일 때 Qualifier 테스트

--- src/main/java/learnspring/discount/FixedDiscountPolicy.java
+++ src/main/java/learnspring/discount/FixedDiscountPolicy.java
@@ -2,7 +2,11 @@ package learnspring.discount;
 
 import learnspring.member.Grade;
 import learnspring.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
+@Component
+@Qualifier("fixedDiscountPolicy")
 public class FixedDiscountPolicy implements DiscountPolicy {
 
     private final int discountFixedAmount = 1000;  // 1000원 고정 할인금액
--- src/main/java/learnspring/discount/RateDiscountPolicy.java
+++ src/main/java/learnspring/discount/RateDiscountPolicy.java
@@ -2,9 +2,11 @@ package learnspring.discount;
 
 import learnspring.member.Grade;
 import learnspring.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+@Qualifier("mainDiscountPolicy")
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private static final int discountPercent = 10;
