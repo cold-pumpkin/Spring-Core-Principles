9cc5627bee079e4b400647607ee7bb17fdd73751
adorno <pch4623@gmail.com>
2020-11-16 20:22:23 +0900

    스프링 라이프 사이클 테스트

--- src/test/java/learnspring/lifecycle/BeanLifecycleTest.java
+++ src/test/java/learnspring/lifecycle/BeanLifecycleTest.java
@@ -1,2 +1,28 @@
-package learnspring.lifecycle;public class BeanLifecycleTest {
+package learnspring.lifecycle;
+
+import org.junit.jupiter.api.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifecycleTest {
+
+    @Test
+    public void lifecycleTest() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();  // 스프링 컨테이너 종료
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http//hello-spring.dev");
+            return networkClient;
+        }
+    }
 }
