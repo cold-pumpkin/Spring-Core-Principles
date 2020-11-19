5f3795f055650aa41e7682d810abd7ec7407befe
adorno <pch4623@gmail.com>
2020-11-19 16:48:21 +0900

    ObjectProvider를 활용해 스프링 컨테이너에서 원하는 프로토타입 빈 찾기

--- src/test/java/learnspring/scope/SingletonWithPrototype.java
+++ src/test/java/learnspring/scope/SingletonWithPrototype.java
@@ -2,6 +2,7 @@ package learnspring.scope;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
@@ -34,19 +35,18 @@ public class SingletonWithPrototype {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);  // ClientBean 생성시점에 PrototypeBean 주입되어 요청마다 같은 빈을 사용하게 됨
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean;   // ClientBean 생성시점에 주입됨
 
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        //private final PrototypeBean prototypeBean;   // ClientBean 생성시점에 주입됨
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
