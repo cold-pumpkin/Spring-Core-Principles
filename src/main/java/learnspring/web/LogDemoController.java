d92741ff6687cf7d72842b6e0f2487f72f79068b
adorno <pch4623@gmail.com>
2020-11-21 23:36:00 +0900

    요청에 따라 로그를 남기는 컨트롤러 작성
    ObjectProvider를 이용해 의존관계 주입 시점이 아닌, HTTP 요청이 들어왔을
    때 MyLogger라는 request scope 빈이 생성될 수 있도록 지연.

--- src/main/java/learnspring/web/LogDemoController.java
+++ src/main/java/learnspring/web/LogDemoController.java
@@ -1,2 +1,31 @@
-package learnspring.web;public class LogDemoController {
+package learnspring.web;
+
+import learnspring.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    //private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        String requestURL = request.getRequestURL().toString();
+        MyLogger myLogger = myLoggerProvider.getObject();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("Controller Test");
+        logDemoService.logic("testId");
+        return "OK";
+    }
 }
